4ec8275f8745e5aa718710c51c1ee6542f7195fe
jihadfathalla <61111967+jihadfathalla@users.noreply.github.com>
2024-07-02 21:26:38 +0300

    Delete utils directory

--- utils/custom_exception_class.py
+++
@@ -1,5 +0,0 @@
-class CustomException(Exception):
-    def __init__(self, status_code, message=None, errors=None):
-        self.message = message
-        self.status_code = status_code
-        self.errors = errors
